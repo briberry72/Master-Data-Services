5fa3fc567bf0e933a58d5b653a60408d5ca9e374
briberry72 <briberry72@users.noreply.github.com>
2017-03-13 16:46:37 -0400

    Powershell Scripts
    Prepare an Application Server for Master Data Services 2016

---
+++ Install Required Windows Features.ps1
@@ -0,0 +1,3 @@
+Install-WindowsFeature Web-Mgmt-Console, AS-NET-Framework, Web-Asp-Net, Web-Asp-Net45, Web-Default-Doc, Web-Dir-Browsing, Web-Http-Errors, Web-Static-Content, Web-Http-Logging, Web-Request-Monitor, Web-Stat-Compression, Web-Dyn-Compression, Web-Filtering, Web-Windows-Auth, NET-Framework-Core, WAS-Process-Model, WAS-NET-Environment, WAS-Config-APIs, Web-Dyn-Compression
+
+Install-WindowsFeature Web-App-Dev, NET-Framework-45-Features -IncludeAllSubFeature –Restart
